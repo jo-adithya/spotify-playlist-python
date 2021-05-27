76d6ee52ecb22a747ced34bf43550f0dc5ac9a3a
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-27 09:53:07 +0700

    create spotify authentication

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from dateutil.parser import parse, ParserError
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 import requests
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from pprint import pprint
+load_dotenv()
 
+# -------------------------------- DATE PARSING --------------------------------- #
 while True:
     date = input('Which year do you want to travel to? : ')
     try:
@@ -11,11 +18,26 @@ while True:
         print('Please give a valid date...')
         continue
 
+# -------------------------------- BEAUTIFUL SOUP -------------------------------- #
 response = requests.get(f'https://www.billboard.com/charts/hot-100/{date}')
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
-titles = soup.find_all(name='span', class_='chart-element__information__song')
+titles = [title.getText() for title in soup.find_all(name='span', class_='chart-element__information__song')]
 
-for i in titles:
-    print(i.getText())
+# ----------------------------------- SPOTIFY ----------------------------------- #
+scope = "playlist-modify-public"
+CLIENT_ID = os.environ.get('SPOTIPY_CLIENT_ID')
+CLIENT_SECRET = os.environ.get('SPOTIPY_CLIENT_SECRET')
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri='http://example.com',
+        scope=scope,
+        show_dialog=True
+    )
+)
+user_id = sp.current_user()['id']
+
